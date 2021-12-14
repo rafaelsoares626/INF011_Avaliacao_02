a3ee5ca3ce341f69a518ed787068f51df2d5bb9e
Veronica Lemos <veulemos@gmail.com>
2021-12-14 13:09:38 -0300

    Update NotificaçãoSMSObsrever

--- AvalDoisPatterns2/src/Model/NotificacaoSMSObserver.java
+++ AvalDoisPatterns2/src/Model/NotificacaoSMSObserver.java
@@ -1,1 +1,23 @@
 package Model;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class NotificacaoSMSObserver implements ObserverIF{
+
+	@Override
+	public void notificar(NotificacaoObserver notificacaoObserver) {
+		SimpleDateFormat fd = new SimpleDateFormat("hh:mm:ss");
+		String data = fd.format(new Date());		
+		System.err.println("\nEnviando SMS..." 
+							+ "\nHora: " + data 
+							+ "\nAssunto: " + notificacaoObserver.getTipoNotificacao()
+							+ "\nConteudo: " + notificacaoObserver.getConteudoNotificacao()
+							+ "\n===========================================================================");		
+	}
+	
+	@Override
+	public String getNomeNotificacao() {
+		return "SMS";
+	}
+
+}
