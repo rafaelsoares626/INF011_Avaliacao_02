eb97716db6c2a02b8ccb701719bbe68581305f9d
Veronica Lemos <veulemos@gmail.com>
2021-12-14 13:06:30 -0300

    Update NotificaçãoWhastappObserver

--- AvalDoisPatterns2/src/Model/NotificacaoWhatsappObserver.java
+++ AvalDoisPatterns2/src/Model/NotificacaoWhatsappObserver.java
@@ -1,1 +1,24 @@
 package Model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class NotificacaoWhatsappObserver implements ObserverIF {
+
+	@Override
+	public void notificar(NotificacaoObserver notificacaoObserver) {
+		SimpleDateFormat fd = new SimpleDateFormat("hh:mm:ss");
+		String data = fd.format(new Date());		
+		System.err.println("\nEnviando Whatsapp..." 
+							+ "\nHora: " + data 
+							+ "\nAssunto: " + notificacaoObserver.getTipoNotificacao()
+							+ "\nConteudo: " + notificacaoObserver.getConteudoNotificacao()
+							+ "\n===========================================================================");		
+	}
+	
+	@Override
+	public String getNomeNotificacao() {
+		return "Whatsapp";
+	}
+
+}
