df2849b6499ebd2138cacdb68b4df2c615457b31
silviocdjm <81332335+silviocdjm@users.noreply.github.com>
2021-12-14 01:58:05 -0300

    Update classe

---
+++ AvalDoisPatterns2/src/Modelo/CursoStateSuspenso.java
@@ -0,0 +1,30 @@
+package Model;
+
+import Model.Curso.Situacao;
+
+public class CursoStateSuspenso extends CursoStateAbstract implements CursoStateIF  {
+	
+	@Override
+	public CursoStateIF ativar() {
+		return new CursoStateEmAndamento();
+	}
+	
+	@Override
+	public CursoStateIF cancelar() {
+		return new CursoStateCancelado();
+	}
+	
+	@Override
+	public Situacao checkpointState(Curso curso) {		
+		Situacao checkpoint = new Curso().new Situacao(curso);
+		for(ObserverIF obs : curso.observers) {			
+			curso.enviarNotificacao("notificacao de salvamento.", obs.getNomeNotificacao(), curso.toString());
+		}		
+		return checkpoint;
+	}
+	
+	@Override
+	public String getEstado() {
+		return "O curso esta suspenso!";
+	}
+}
